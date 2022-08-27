c21e0f1992074e1af3ddc339028ecc2ad4448e09
Tim Moliterno <tmoliter@gmail.com>
2022-08-27 11:08:39 -0400

    cleanup

--- MapParser.cpp
+++ MapParser.cpp
@@ -9,38 +9,25 @@ Thing *parse_thing(ifstream &mapData, ThingList &thingList) {
         ThingData newTD;
         Thing::write_thing_datum(mapData, newTD);
         thingList.addThing(new Thing(newTD));
-        return thingList.lastThing();
     }
     if(next == 'P') {
         FieldPlayerData newTD;
         FieldPlayer::write_thing_datum(mapData, newTD);
         thingList.addThing(new FieldPlayer(newTD));
-        return thingList.lastThing();
     }
-    return NULL;
+    return thingList.lastThing();
 }
-
-// void parse_component(ifstream &mapData, ThingList &thingList) {
-
-// }
 
 void parse_map(ThingList &thingList, Camera *c) {
     ifstream mapData;
     mapData.open("./maps/map.txt");
 
     Thing *focus = parse_thing(mapData, thingList);
-
     Camera::parse_camera(mapData, c);
-
     c->init(&focus->x, &focus->y);
 
     do {
         parse_thing(mapData, thingList);
-        // if(mapData.peek() == ',') {
-        //     while(mapData.get() == ',') {
-        //         parse_component(mapData,thingList);
-        //     }
-        // }
     } while (mapData.get() == '\n');
     thingList.initThings(&c->x,&c->y,c->renderer);
     mapData.close();
