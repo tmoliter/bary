7178925a8bdab15e1509939bc4e71c151440d13f
Tim Moliterno <tmoliter@gmail.com>
2023-12-31 21:17:59 -0500

    add existing buildings to new setup

--- scenes/burg/newmap.lua
+++ scenes/burg/newmap.lua
@@ -21,5 +21,20 @@ return {
             x = 800,
             y = 500
         },
+        {
+            name = "sailorShack",
+            x = 723,
+            y = 1037,
+        },
+        {
+            name = "sailorShack",
+            x = 1500,
+            y = 1037,
+        },
+        {
+            name = "genrlStore",
+            x = 621,
+            y = 510,
+        }
     } 
 }
